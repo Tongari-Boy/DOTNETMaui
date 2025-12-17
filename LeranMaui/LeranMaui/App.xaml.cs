fa789aaf61d6d2403154b947dc9ad1c60bfe3d37
Tongari-Boy <tongariboy@outlook.com>
2025-12-17 14:55:58 +0900

    first commit

---
+++ LeranMaui/LeranMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeranMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
